6c3019b786ea3b79da50245b843de5c2e39be367
Jonathan Regeimbal <jonathan@regeimbal.net>
2026-03-08 14:19:26 -0600

    Fix test naming

--- test/chat_message_list_test.dart
+++ test/chat_message_list_test.dart
@@ -4,7 +4,7 @@ import 'package:provider/provider.dart';
 import 'package:translation_intelligence/controllers/speech_controller.dart';
 import 'package:translation_intelligence/widgets/chat_message.dart';
 
-import 'test_speech_controller.dart';
+import 'speech_controller_stub.dart';
 
 void main() {
   group('ChatMessageList', () {
